53add02f7d4a717d376e6ffe2280008ad03e4903
dadig <36836594+dadig@users.noreply.github.com>
2018-03-20 22:22:41 +0800

    Update replace_table.py

--- replace_table.py
+++ replace_table.py
@@ -1,7 +1,7 @@
  #replace_table.py
  #replace_table 功能是在docx文件中保持表格中字体格式不变替换相应内容
- def replace_table(self,old_text,new_text):
-        for table in self.doc.tables:
+ def replace_table(old_text,new_text):
+        for table in doc.tables:
     	    for row in table.rows:
                 for cell in row.cells:
                     if old_text in cell.text:
